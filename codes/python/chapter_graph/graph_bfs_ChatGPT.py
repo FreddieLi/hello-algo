4fc9840c0c9db0f0f89a1c8d1437a89d7c604a37
FreddieLi <b09613@gmail.com>
2023-09-30 13:43:01 +0800

    Create bfs by chatgpt
    try not to use the vertie

---
+++ codes/python/chapter_graph/graph_bfs_ChatGPT.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=' ')
+            visited.add(node)
+            for neighbour in graph[node]:
+                if neighbour not in visited:
+                    queue.append(neighbour)
+
+# Example graph as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+print("Breadth-First Search:")
+bfs(graph, 'A')
